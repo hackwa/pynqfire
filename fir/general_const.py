1d6b21914cf0d2a89bd0fbf036a02fa5b93493d0
hackwa <anurag.dubey@colorado.edu>
2016-05-24 11:26:42 -0600

    bs search path

--- fir/general_const.py
+++ fir/general_const.py
@@ -1,2 +1,6 @@
-BITFILE = "filter.bit"
-LIBRARY = "libfir.so"
+import os
+
+BS_SEARCH_PATH = os.path.dirname(os.path.realpath(__file__))
+
+BITFILE = BS_SEARCH_PATH + "/filter.bit"
+LIBRARY = BS_SEARCH_PATH + "/libfir.so"
